f73ddd1a16aac5c3e2eb17bea6d1ec9a7119cbbe
Lokesh Sirna <lokesh_sirna@epam.com>
2022-10-07 11:26:04 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CustomNavBar from "./BookStoreComp/CustomNavBar"
+import { BrowserRouter as BR, Route, Routes, useParams } from 'react-router-dom';
+import Home from './BookStoreComp/Home';
+import MyOrders from './BookStoreComp/MyOrders';
+import Cart from './BookStoreComp/Cart';
+import Routing2 from './Routing/Routing2';
+// import HomeContainer from './reduxContainers/HomeContainer';
+export default function App() {
+
+  return (
+    <div>
+      <CustomNavBar/>
+    </div>
+  )
+}
